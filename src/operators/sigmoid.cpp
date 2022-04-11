83b3e6752f0334cf604be0ba89760da2ef5b48bf
CnybTsneg <984943763@qq.com>
2022-04-11 18:21:53 +0800

    add file note

--- src/operators/sigmoid.cpp
+++ src/operators/sigmoid.cpp
@@ -1,3 +1,38 @@
+/******************************************************************
+ * PyTorch to executable program (torch2exe).
+ * Copyright © 2022 Zhiwei Zeng
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the “Software”), to deal 
+ * in the Software without restriction, including without limitation the rights 
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+ * copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all 
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ * SOFTWARE.
+ *
+ * This file is part of torch2exe.
+ *
+ * @file
+ * @brief Sigmoid activation operator.
+ *
+ * @author Zhiwei Zeng
+ * @email chinaybtseng@gmail.com
+ * @version 1.0
+ * @date April 11, 2022
+ * @license The MIT License (MIT)
+ *
+ ******************************************************************/
+
 #include <vector>
 
 #include <NvInfer.h>
